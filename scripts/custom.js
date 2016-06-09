2fdfa7ae7cf6b1f4dde4cb0b5ee3b1d78d0f5087
JaclynNoga <jaclyn.noga@gmail.com>
2016-06-09 10:11:40 -0400

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -57,3 +57,25 @@ $("#about").click(function () {
     scrollToAnchor('about');
 });
 //end scroll
+
+//smooth scroll for navigation
+$("li a").click(function () {
+    var navLinks = $(this).attr("href");
+    $("body").animate({ scrollTop: $(navLinks).offset().top }, "slow");
+    return false;
+});
+
+
+// ===== Scroll to Top ==== 
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 550) {        // If page is scrolled more than 550px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function () {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0                       // Scroll to top of body
+    }, 500);
+});
